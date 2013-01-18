fe6c1391dae2bf4b802db732b97d3c3c04be2cb3
Greg Allen <me@jga.me>
2013-01-17 17:47:00 -0800

    [tests] basic tests

--- test/fidel-template.test.js
+++ test/fidel-template.test.js
@@ -1,1 +1,26 @@
 //fidel-template.test.js
+
+
+suite('fidel-template', function() {
+
+  test('exists', function() {
+    assert.equal(typeof Fidel.prototype.render, 'function');
+  });
+
+  test('render template', function() {
+
+    $.declare('widget', {
+      templateId: 'tmpl-main',
+      init: function() {
+        this.render({ name: 'Bob' });
+      }
+    });
+    var el = $('#fixture .widget');
+    el.widget();
+    assert.equal(el.text(), 'Hello Bob');
+
+  });
+
+  test('render template to a specific element');
+
+});
